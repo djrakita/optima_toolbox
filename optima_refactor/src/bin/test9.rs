d491057c316b4f91d57aa65f3ec4598f65b40713
Xiatao Sun <sunxiatao@gmail.com>
2023-12-29 11:40:53 -0500

    manual allocate and deallocate memory for ffi wrapper

--- optima_refactor/src/bin/test9.rs
+++ optima_refactor/src/bin/test9.rs
@@ -1,4 +1,4 @@
-use ad_trait::differentiable_function::{FiniteDifferencing2, ForwardADMulti2, ReverseAD2};
+use ad_trait::differentiable_function::{FiniteDifferencing, ForwardADMulti, ReverseAD};
 use ad_trait::forward_ad::adfn::adfn;
 use nalgebra::Isometry3;
 use optima_3d_spatial::optima_3d_pose::O3DPose;
@@ -6,8 +6,8 @@ use optima_bevy::optima_bevy_utils::robotics::BevyRoboticsTrait;
 use optima_bevy::OptimaBevyTrait;
 use optima_interpolation::InterpolatorTrait;
 use optima_interpolation::splines::{InterpolatingSpline, InterpolatingSplineType};
-use optima_optimization2::{DiffBlockOptimizerTrait, OptimizerOutputTrait};
-use optima_optimization2::open::SimpleOpEnOptimizer;
+use optima_optimization::{DiffBlockOptimizerTrait, OptimizerOutputTrait};
+use optima_optimization::open::SimpleOpEnOptimizer;
 use optima_proximity::pair_group_queries::{OwnedEmptyParryFilter, OwnedEmptyToProximityQry, OwnedParryDistanceGroupSequenceFilter, ParryDistanceGroupSequenceFilterArgs, ProximityLossFunction};
 use optima_proximity::pair_queries::{ParryDisMode, ParryShapeRep};
 use optima_proximity::proxima::{OwnedParryProximaAsProximityQry, PairGroupQryArgsParryProxima, ProximaTermination};
@@ -17,12 +17,13 @@ use optima_robotics::robotics_optimization::robotics_optimization_ik::{Different
 fn main() {
     let robot = ORobotDefault::load_from_saved_robot("xarm7_with_gripper_and_rail_8dof");
 
+
     // let fq = OwnedParryDistanceGroupSequenceFilter::new(ParryDistanceGroupSequenceFilterArgs::new(vec![ParryShapeRep::BoundingSphere, ParryShapeRep::OBB, ParryShapeRep::Full], vec![], 0.6, true, ParryDisMode::ContactDis));
     let fq = OwnedEmptyParryFilter::new(());
     // let q = OwnedParryProximaAsProximityQry::new(PairGroupQryArgsParryProxima::new(ParryShapeRep::Full, true, false, ProximaTermination::MaxError(0.1), ProximityLossFunction::Hinge, 15.0, 0.6));
     let q = OwnedEmptyToProximityQry::new(());
     let initial_state = vec![0.3; 8];
-    let db = robot.get_ik_differentiable_block(ForwardADMulti2::<adfn<8>>::new(), fq, q, None, &initial_state, vec![19], 0.09, 0.6, 1.0, 0.0, 0.0, 0.0, 0.0);
+    let db = robot.get_ik_differentiable_block(ForwardADMulti::<adfn<8>>::new(), fq, q, None, &initial_state, vec![19], 0.09, 0.6, 1.0, 0.0, 0.0, 0.0, 0.0);
     let o = SimpleOpEnOptimizer::new(robot.get_dof_lower_bounds(), robot.get_dof_upper_bounds(), 0.001);
 
     let mut solutions = vec![];
@@ -37,6 +38,7 @@ fn main() {
         let res = o.optimize_unconstrained(&curr_solution, &db);
         println!("{:?}", res.solver_status().solve_time());
         curr_solution = res.x_star().to_vec();
+        println!("Current solution: {:?}", curr_solution);
         db.update_ik_pose(0, Isometry3::from_constructors(&[0.0, 0.0, 0.0001], &[0.,0.,0.]), IKGoalUpdateMode::GlobalRelative);
         db.update_prev_states(curr_solution.clone());
     }
